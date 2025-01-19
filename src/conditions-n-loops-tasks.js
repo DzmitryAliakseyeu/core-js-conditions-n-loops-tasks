0509ffe27e7592e9750dab6293df1d4337387616
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-01-19 17:34:36 +0100

    feat: tealization shuffleChar function (Sun, Jan 19, 2025 17:34:45 PM)

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -442,7 +442,6 @@ function sortByAsc(arr) {
     return arC;
   }
 
-  // Partition function for quick sort
   function partition(arr1, left, right) {
     const pivot = arr1[right];
     let i = left - 1;
@@ -487,8 +486,34 @@ function sortByAsc(arr) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  const { length } = str;
+  let strC = str;
+  let result = '';
+  let newStr = '';
+  let odd = '';
+  let even = '';
+
+  for (let i = 0; i < iterations; i += 1) {
+    if (i > 0) {
+      strC = newStr;
+      even = '';
+      odd = '';
+    }
+    for (let j = 0; j < length; j += 1) {
+      if (j % 2 === 0) {
+        even += strC[j];
+      } else {
+        odd += strC[j];
+      }
+
+      newStr = even + odd;
+    }
+
+    result = newStr;
+  }
+
+  return result;
 }
 
 /**
